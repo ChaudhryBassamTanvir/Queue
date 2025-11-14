efc82bb79539af52aeb7681082efe6ffd39855cd
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-14 12:51:19 +0500

    Updated

--- CircularQueue.java
+++ CircularQueue.java
@@ -1,6 +1,5 @@
-public class QueuesClass {
-
-
+public class CircularQueue {
+    
     static class Queue{
         static int arr[];
         static int size;
@@ -30,32 +29,25 @@ if (isFull()) {
 if (front ==-1) {
 front=0;
 }
-
-
-        if (rear==size-1) {
-            System.out.println("Full queue");
-            return;
-        }
-        rear++;
-        arr[rear]=data;
-
+   
+    rear=(rear+1)%size;
+    arr[rear]=data;
     }
 
-    //dequeue or remove
+    //dequeue or remove O(1)
 public static int remove(){
     if (isEmpty()) {
         return -1;
 
         
     }
-int front = arr[0];
-for (int i = 0; i <rear; i++) {
-arr[i]=arr[i+1];
-
+int result = arr[front];
+if (rear==front ) {
+    rear=front=-1;
+}else{
+    front=(front+1)%size;
 }
-rear--;
-
-return front;
+return result;
 
 }
 
@@ -66,30 +58,27 @@ public static int peek(){
 
         
     }
-return arr[0];
+return arr[front];
 }
     }
 
-
-
-    public static void main(String[] args) {
-     //enque Add
-     // Dequeue Remove
-     //Front Peek
-     
-
+public static void main(String[] args) {
      Queue q = new Queue(5);
      q.add(1);
      q.add(2);
      q.add(3);
      q.add(4);
-
+     q.add(5);
+     System.out.println();
+     System.out.println(q.remove());
+    q.add(6);
+    System.out.println(q.remove());
+    q.add(7);
 
      while(!q.isEmpty()){
         System.out.println(q.peek());
         q.remove();
      }
      
-
-    }
+}
 }
