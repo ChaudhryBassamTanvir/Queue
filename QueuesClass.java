c754dfb7aa4f47b70a530cd94db76b28a7a6a02b
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-14 12:40:54 +0500

    Updated

--- QueuesClass.java
+++ QueuesClass.java
@@ -5,30 +5,46 @@ public class QueuesClass {
         static int arr[];
         static int size;
         static int rear = -1;
-
+static int front=-1;
         Queue(int n){
             arr = new int[n];
             this.size = n;
         }
 
         public static boolean isEmpty(){
-            return rear==-1;
+            return rear==-1 && front ==-1;
         }
-        
+        public static boolean isFull(){
+            return (rear+1)%size==front;
+        }
+
+
     //add or enqueue
     public static void add(int data){
+if (isFull()) {
+    System.out.println("Full queue");
+    return;
+    
+}
+//f
+if (front ==-1) {
+front=0;
+}
+
+
         if (rear==size-1) {
             System.out.println("Full queue");
             return;
         }
-        rear++;
-        arr[rear]=data;
-
+        // rear++;
+        // arr[rear]=data;
+    rear=(rear+1)%size;
+    arr[rear]=data;
     }
 
     //dequeue or remove
 public static int remove(){
-    if (!isEmpty()) {
+    if (isEmpty()) {
         return -1;
 
         
@@ -46,7 +62,7 @@ return front;
 
 //peek
 public static int peek(){
-    if (!isEmpty()) {
+    if (isEmpty()) {
         return -1;
 
         
